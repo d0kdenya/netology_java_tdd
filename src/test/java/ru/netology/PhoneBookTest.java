043e22b1f6c9809a297dee7675180329980f4b8c
Dan Losev <shocker.nn@mail.ru>
2025-09-10 00:05:31 +0300

    Stub and tests for add

--- src/test/java/ru/netology/PhoneBookTest.java
+++ src/test/java/ru/netology/PhoneBookTest.java
@@ -1,4 +1,21 @@
 package ru.netology;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PhoneBookTest {
+
+    @Test
+    public void testAdd() {
+        PhoneBook phoneBook = new PhoneBook();
+        int size = phoneBook.add("Alice", "12345");
+        assertEquals(1, size);
+
+        size = phoneBook.add("Bob", "67890");
+        assertEquals(2, size);
+
+        size = phoneBook.add("Alice", "99999");
+        assertEquals(2, size);
+    }
 }
